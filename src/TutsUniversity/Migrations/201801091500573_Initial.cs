687016a7fb27ec3190635845399292627502359e
Gary Stonerock II <GaryStonerockII@gmail.com>
2018-01-09 10:04:18 -0500

    Starting fresh with migrations

--- src/TutsUniversity/Migrations/201801091500573_Initial.cs
+++ src/TutsUniversity/Migrations/201801091500573_Initial.cs
@@ -1,12 +1,25 @@
-using System.Data.Entity.Migrations;
-
 namespace TutsUniversity.Migrations
 {
-    public partial class ComplexDataModel : DbMigration
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
     {
         public override void Up()
         {
-            RenameColumn(table: "dbo.Student", name: "FirstMidName", newName: "FirstName");
+            CreateTable(
+                "dbo.Course",
+                c => new
+                    {
+                        CourseID = c.Int(nullable: false),
+                        Title = c.String(maxLength: 50),
+                        Credits = c.Int(nullable: false),
+                        DepartmentID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.CourseID)
+                .ForeignKey("dbo.Department", t => t.DepartmentID, cascadeDelete: true)
+                .Index(t => t.DepartmentID);
+            
             CreateTable(
                 "dbo.Department",
                 c => new
@@ -16,19 +29,22 @@ namespace TutsUniversity.Migrations
                         Budget = c.Decimal(nullable: false, storeType: "money"),
                         StartDate = c.DateTime(nullable: false),
                         InstructorID = c.Int(),
+                        RowVersion = c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"),
                     })
                 .PrimaryKey(t => t.DepartmentID)
-                .ForeignKey("dbo.Instructor", t => t.InstructorID)
+                .ForeignKey("dbo.Person", t => t.InstructorID)
                 .Index(t => t.InstructorID);
             
             CreateTable(
-                "dbo.Instructor",
+                "dbo.Person",
                 c => new
                     {
                         ID = c.Int(nullable: false, identity: true),
                         LastName = c.String(nullable: false, maxLength: 50),
                         FirstName = c.String(nullable: false, maxLength: 50),
-                        HireDate = c.DateTime(nullable: false),
+                        HireDate = c.DateTime(),
+                        EnrollmentDate = c.DateTime(),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
                     })
                 .PrimaryKey(t => t.ID);
             
@@ -40,8 +56,23 @@ namespace TutsUniversity.Migrations
                         Location = c.String(maxLength: 50),
                     })
                 .PrimaryKey(t => t.InstructorID)
-                .ForeignKey("dbo.Instructor", t => t.InstructorID)
+                .ForeignKey("dbo.Person", t => t.InstructorID)
                 .Index(t => t.InstructorID);
+            
+            CreateTable(
+                "dbo.Enrollment",
+                c => new
+                    {
+                        EnrollmentID = c.Int(nullable: false, identity: true),
+                        CourseID = c.Int(nullable: false),
+                        StudentID = c.Int(nullable: false),
+                        Grade = c.Int(),
+                    })
+                .PrimaryKey(t => t.EnrollmentID)
+                .ForeignKey("dbo.Course", t => t.CourseID, cascadeDelete: true)
+                .ForeignKey("dbo.Person", t => t.StudentID, cascadeDelete: true)
+                .Index(t => t.CourseID)
+                .Index(t => t.StudentID);
             
             CreateTable(
                 "dbo.CourseInstructor",
@@ -52,44 +83,34 @@ namespace TutsUniversity.Migrations
                     })
                 .PrimaryKey(t => new { t.CourseID, t.InstructorID })
                 .ForeignKey("dbo.Course", t => t.CourseID, cascadeDelete: true)
-                .ForeignKey("dbo.Instructor", t => t.InstructorID, cascadeDelete: true)
+                .ForeignKey("dbo.Person", t => t.InstructorID, cascadeDelete: true)
                 .Index(t => t.CourseID)
                 .Index(t => t.InstructorID);
-
-            // Create  a department for course to point to.
-            Sql("INSERT INTO dbo.Department (Name, Budget, StartDate) VALUES ('Temp', 0.00, GETDATE())");
-            //  default value for FK points to department created above.
-            AddColumn("dbo.Course", "DepartmentID", c => c.Int(nullable: false, defaultValue: 1));
-            //AddColumn("dbo.Course", "DepartmentID", c => c.Int(nullable: false));
-
-            AlterColumn("dbo.Course", "Title", c => c.String(maxLength: 50));
-            AlterColumn("dbo.Student", "LastName", c => c.String(nullable: false, maxLength: 50));
-            AlterColumn("dbo.Student", "FirstName", c => c.String(nullable: false, maxLength: 50));
-            CreateIndex("dbo.Course", "DepartmentID");
-            AddForeignKey("dbo.Course", "DepartmentID", "dbo.Department", "DepartmentID", cascadeDelete: true);
+            
         }
         
         public override void Down()
         {
-            DropForeignKey("dbo.CourseInstructor", "InstructorID", "dbo.Instructor");
+            DropForeignKey("dbo.CourseInstructor", "InstructorID", "dbo.Person");
             DropForeignKey("dbo.CourseInstructor", "CourseID", "dbo.Course");
+            DropForeignKey("dbo.Enrollment", "StudentID", "dbo.Person");
+            DropForeignKey("dbo.Enrollment", "CourseID", "dbo.Course");
             DropForeignKey("dbo.Course", "DepartmentID", "dbo.Department");
-            DropForeignKey("dbo.Department", "InstructorID", "dbo.Instructor");
-            DropForeignKey("dbo.OfficeAssignment", "InstructorID", "dbo.Instructor");
+            DropForeignKey("dbo.Department", "InstructorID", "dbo.Person");
+            DropForeignKey("dbo.OfficeAssignment", "InstructorID", "dbo.Person");
             DropIndex("dbo.CourseInstructor", new[] { "InstructorID" });
             DropIndex("dbo.CourseInstructor", new[] { "CourseID" });
+            DropIndex("dbo.Enrollment", new[] { "StudentID" });
+            DropIndex("dbo.Enrollment", new[] { "CourseID" });
             DropIndex("dbo.OfficeAssignment", new[] { "InstructorID" });
             DropIndex("dbo.Department", new[] { "InstructorID" });
             DropIndex("dbo.Course", new[] { "DepartmentID" });
-            AlterColumn("dbo.Student", "FirstName", c => c.String(maxLength: 50));
-            AlterColumn("dbo.Student", "LastName", c => c.String(maxLength: 50));
-            AlterColumn("dbo.Course", "Title", c => c.String());
-            DropColumn("dbo.Course", "DepartmentID");
             DropTable("dbo.CourseInstructor");
+            DropTable("dbo.Enrollment");
             DropTable("dbo.OfficeAssignment");
-            DropTable("dbo.Instructor");
+            DropTable("dbo.Person");
             DropTable("dbo.Department");
-            RenameColumn(table: "dbo.Student", name: "FirstName", newName: "FirstMidName");
+            DropTable("dbo.Course");
         }
     }
 }
