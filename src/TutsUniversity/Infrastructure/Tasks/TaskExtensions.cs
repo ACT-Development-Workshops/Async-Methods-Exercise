aa8e8d5b8c1733ca306a33139cd5635b0ad42775
Gary Stonerock II <GaryStonerockII@gmail.com>
2018-01-09 16:02:49 -0500

    Adding WaitForAllAndThenAggregateResults

---
+++ src/TutsUniversity/Infrastructure/Tasks/TaskExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutsUniversity.Infrastructure.Tasks
+{
+    public static class TaskExtensions
+    {
+        public static async Task WaitForAllAndThenAggregateResults<TSource>(this IEnumerable<TSource> source, Func<TSource, Task> selector)
+        {
+            var tasks = source.Select(selector);
+            await tasks.WaitForAllAndThenAggregateResults();
+        }
+
+        private static async Task WaitForAllAndThenAggregateResults(this IEnumerable<Task> tasks)
+        {
+            var compositeTask = Task.WhenAll(tasks);
+            await compositeTask.ContinueWith(_ => { }, TaskContinuationOptions.ExecuteSynchronously).ConfigureAwait(false);
+            compositeTask.Wait();
+        }
+    }
+}
