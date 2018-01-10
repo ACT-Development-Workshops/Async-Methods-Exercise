3a56171377f022ee065ea744baf14818446b28e3
Gary Stonerock II <GaryStonerockII@gmail.com>
2018-01-10 13:59:08 -0500

    Moving TaskExtensions to Infrastructure root

--- src/TutsUniversity/Infrastructure/TaskExtensions.cs
+++ src/TutsUniversity/Infrastructure/TaskExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TutsUniversity.Infrastructure.Tasks
+namespace TutsUniversity.Infrastructure
 {
     public static class TaskExtensions
     {
